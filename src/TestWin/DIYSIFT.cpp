b4410d81d09bc104eb9e1892661c968ed93e9b1f
s <s>
2018-02-06 16:47:24 +0800

    ready to integrate into theia-sdioi, leave 4 key line: SiftGPU sift; sift.CreateContextGL(); sift.RunSIFT("../data/800-1.jpg"); sift.GetFeatureNum();

--- src/TestWin/DIYSIFT.cpp
+++ src/TestWin/DIYSIFT.cpp
@@ -36,29 +36,16 @@ int main()
 {
 	SiftGPU sift;
 
-    vector<float > descriptors1(1), descriptors2(1);
-    vector<SiftGPU::SiftKeypoint> keys1(1), keys2(1);    
-    int num1 = 0, num2 = 0;
-
     //Create a context for computation, and SiftGPU will be initialized automatically 
     //The same context can be used by SiftMatchGPU
-    if(sift.CreateContextGL() != SiftGPU::SIFTGPU_FULL_SUPPORTED) return 0;
+    if(sift.CreateContextGL() != SiftGPU::SIFTGPU_FULL_SUPPORTED) 
+		return 0;
 
     if(sift.RunSIFT("../data/800-1.jpg"))
     {
-        //Call SaveSIFT to save result to file, the format is the same as Lowe's
-        //sift->SaveSIFT("../data/800-1.sift"); //Note that saving ASCII format is slow
-
         //get feature count
-        num1 = sift.GetFeatureNum();
-
-        //allocate memory
-        keys1.resize(num1);    descriptors1.resize(128*num1);
-
-        //reading back feature vectors is faster than writing files
-        //if you dont need keys or descriptors, just put NULLs here
-        sift.GetFeatureVector(&keys1[0], &descriptors1[0]);
-        //this can be used to write your own sift file.            
+        int num = sift.GetFeatureNum();
+		std::cout << "feature number is : " << num;
     }
 
 
